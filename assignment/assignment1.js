839b9c1d47b8a660ad7590025fbed6f572709b37
azrybhkahmad <azrybhkahmad@gmail.com>
2020-06-04 22:31:47 +0800

    move
    again | move it to a folder to reduce complication with other exercise

--- assignment/assignment1.js
+++ assignment/assignment1.js
@@ -1,19 +1,25 @@
 const http = require('http');
 
 const server =http.createServer((req, res) => {
+  // request URL (whatever the user enter after 'localhost:3123')
     const url = req.url
+    // example below (localhost:3123/)
         if (url === '/') {
+          // start of html
         res.write('<html>');
         res.write('<head>Username assignment</head>');
+        // Creating of form in the website
         res.write('<body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Submit</button>');
         res.write('</html>');
     }
     if (url === '/users') {
         res.setHeader('Content-Type', 'text/html');
+        // start of html
         res.write ('<html>');
         res.write ('<head>Username assignment</head>');
         res.write ('<body><ul><li>User 1<li><li>User 2</li></ul></body>');
         res.write ('</html>');
+        // end request
         return res.end();
     }
     if (url === '/create-user') {
@@ -24,6 +30,7 @@ const server =http.createServer((req, res) => {
         
         req.on('end', () => {
           const parsedBody = Buffer.concat(body).toString();
+          // username=(*data user entered)
           console.log(parsedBody.split('=')[1]); 
           
         });
